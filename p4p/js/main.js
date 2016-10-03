2008a1917156f39248632cff5395ee2bd6eb7ee5
Dmitry Verenich <bmdi.sp.lwr@gmail.com>
2016-10-03 19:16:18 +0300

    Add files via upload

--- p4p/js/main.js
+++ p4p/js/main.js
@@ -1,15 +1,15 @@
 /**
  * Created by Wide-Web on 28.09.2016.
  */
+
 $(function () {
     var owl = $('#owl-head');
-
 
     owl.owlCarousel({
 
         nav: false,
-        slideSpeed : 10,
-        paginationSpeed : 10,
+        slideSpeed: 10,
+        paginationSpeed: 10,
         items: 1,
         loop: true,
         dots: true
@@ -18,37 +18,31 @@ $(function () {
 
 
 $(function () {
-    if( jQuery(".toggle .toggle-title").hasClass('active') ){
+    if (jQuery(".toggle .toggle-title").hasClass('active')) {
         jQuery(".toggle .toggle-title.active").closest('.toggle').find('.toggle-inner').show();
     }
-    jQuery(".toggle .toggle-title").click(function(){
-        if( jQuery(this).hasClass('active') ){
+    jQuery(".toggle .toggle-title").click(function () {
+        if (jQuery(this).hasClass('active')) {
             jQuery(this).removeClass("active").closest('.toggle').find('.toggle-inner').slideUp(200);
         }
-        else{	jQuery(this).addClass("active").closest('.toggle').find('.toggle-inner').slideDown(200);
+        else {
+            jQuery(this).addClass("active").closest('.toggle').find('.toggle-inner').slideDown(200);
         }
     });
 });
 
-$(document).ready(function(){
-    $('.menutoggle,.menumobile>a').click (function() {
+
+$(document).ready(function () {
+    $('.menutoggle, .menumobile>a').click(function () {
         $('.menumobile').toggleClass("menuoff");
         $("html,body").toggleClass("noscroll");
         $('.overlay').toggleClass("overlay-active");
     });
 
-    $('.banktoggle,.bankmobile>a').click (function() {
+    $('.banktoggle, .bankmobile>a').click(function () {
         $('.bankmobile').toggleClass("menuoff");
         $("html,body").toggleClass("noscroll");
         $('.overlay').toggleClass("overlay-active");
     });
-    
-});
 
-
-
-$(document).ready(function() {
-    $('#lightgallery').lightGallery({
-        pager: true
-    });
 });
