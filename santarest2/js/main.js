0935e7a65f6888ddaf886b2c578c3601e62fdc4a
Dmitry Verenich <bmdi.sp.lwr@gmail.com>
2017-02-20 14:16:22 +0300

    Add files via upload

---
+++ santarest2/js/main.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Wide-Web on 15.02.2017.
+ */
+$(function () {
+    $('.owl-carousel-only-phone').owlCarousel({
+        loop: true,
+        margin: 10,
+        items: 1
+    });
+});
+
+/* Menu */
+
+$(function () {
+    $('.dropdown').click(function (e) {
+        e.stopPropagation();
+        $('.nav-dropdown').toggle();
+    });
+
+    $('html').click(function () {
+        $('.nav-dropdown').hide();
+    });
+
+
+
+    $('.js-nav-more').on('click', function(){
+        $('.js-sub-nav').slideToggle();
+        $('.js-nav-more i').toggleClass('cross');
+        $('.catalog-nav').toggleClass('blurred');
+    });
+
+
+});
+
+
+
