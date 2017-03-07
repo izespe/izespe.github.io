733192c4248efe93d21a53efa733f046d97d3d2d
Dmitry Verenich <bmdi.sp.lwr@gmail.com>
2017-03-07 18:15:49 +0300

    Add files via upload

--- santarest2/js/main.js
+++ santarest2/js/main.js
@@ -40,7 +40,25 @@ $(function () {
         $('.js-nav-more i').removeClass('cross');
         $('.catalog-nav').removeClass('blurred');
     });
+    $(window).scroll(function() {
 
+        if($(this).scrollTop() != 0) {
 
+            $('#toTop').fadeIn();
+
+        } else {
+
+            $('#toTop').fadeOut();
+
+        }
+
+    });
+
+    $('#toTop').click(function() {
+
+        $('body,html').animate({scrollTop:0},800);
+
+    });
 
 });
+
