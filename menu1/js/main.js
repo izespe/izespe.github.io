9d814b110009ca58a561aaf1bd0406872f789851
Dmitry Verenich <bmdi.sp.lwr@gmail.com>
2017-03-22 15:47:25 +0300

    delete tests

--- menu1/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.menutoggle').click (function() {
-        $('.menumobile').toggleClass("menuoff");
-        $('.menutoggle').toggleClass("buttonmove");
-        $("html,body").toggleClass("noscroll");
-    });
-});
