60fd9c01904c58f9913ba17cfce093b3e96f896b
Dmitry Verenich <bmdi.sp.lwr@gmail.com>
2017-06-01 18:22:14 +0700

    Add files via upload

--- admin/js/main.js
+++ admin/js/main.js
@@ -1,3 +1,15 @@
-/**
- * Created by Wide-Web on 31.05.2017.
- */
+/* Progressive .js enhancement - not mandatory */
+/* This is only here to set initial navigation state from anchors */
+var pushNav = document.getElementsByClassName('redaktor push nav')[0];
+document.body.dataset.has_js = 'true';
+document.body.dataset.initial = '1';
+Array.prototype.forEach.call(pushNav.getElementsByTagName('a'), function(el, i){
+    if (window.location.hash === el.getAttribute("href")) {
+        document.body.dataset.initial = (i+1).toString();
+    }
+});
+var endInit = function (event) {
+    delete document.body.dataset.initial;
+    pushNav.removeEventListener('click', endInit, false);
+};
+pushNav.addEventListener('click', endInit, false);
